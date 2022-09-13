c113867474607ccde1d423156d04bef7cbf031a9
liudi <995492682@qq.com>
2022-09-13 14:51:10 +0800

    requests.get/post

--- demo_file.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-
-# def mysql():
-
-db = pymysql.connect(host="rm-2ze61bh4321555111.mysql.rds.aliyuncs.com", user="root", password="poiuy)(*&^123", db="chengdu_password")
-cursor = db.cursor()
-print(cursor)
