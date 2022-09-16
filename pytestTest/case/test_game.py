69e49ccadc80861752392bdffaddaf309ed9a3dd
liudi <995492682@qq.com>
2022-09-16 18:15:34 +0800

    Apitest

--- pytestTest/case/test_game.py
+++ pytestTest/case/test_game.py
@@ -21,29 +21,3 @@ def test_01_login():
     token = json.loads(res.text)["data"]["user"]["token"]
     save_token(res)
     return token
-
-
-def test_02_lupin():
-    u = "http://hapi.test.hualiantv.com/user/loginByCode"
-    d = {"mobile": "13694000445",
-         "code": "0000",
-         "platform": "h5"}
-    res = requests.post(url=u, data=d)
-    res_json = json.dumps(json.loads(res.text), indent=4, ensure_ascii=False)
-    assert res.status_code == 200
-    assert json.loads(res.text).get("errno") == 0
-    assert json.loads(res.text).get("errmsg") == "请求成功"
-    sql_ssh = "select * from user where rid = '{}'".format(d.get("mobile"))
-    assert len(SSH_select(sql_ssh)) != 0
-    print(read_token())
-
-
-"""
-活动，游戏中奖率，压测
-注册登陆（三方登陆），私信  上下麦 弹幕，（付费弹幕，查库）。首页，动态页，
-error_num
-
-
-收送礼，加解密
-
-"""
